13f0018ad5b7feafd9ceae2b1c44c26ebbfa48ec
Mohammed cha <40530012+Mohammedcha@users.noreply.github.com>
2021-07-07 00:03:02 +0100

    Update re-skinning.php

--- re-skinning.php
+++ re-skinning.php
@@ -1,12 +1,12 @@
 <?php 
 
 /*
-Plugin Name: Re-skinning bot splitter
+Plugin Name: Re-skinning URL Splitter
 Plugin URI: https://re-skinning.com/
-Description: Re-skinning BotCloaker : automatically coloak bots and add a SMART button at the end of any post. by Mohammed Cha / Re-Skinnig Group
-Version: 1.0
+Description: Re-skinning URL Splitter : by Mohammed Cha / Re-Skinning Group <3
+Version: 1.0.3
 Author: Mohammed Cha
-Author URI: https://cutt.ly/bmkzGGi
+Author URI: https://github.com/Mohammedcha/Re-skinning_url_splitter
 License: GPLv2 or later
 Text Domain: Re-skinning
 */
